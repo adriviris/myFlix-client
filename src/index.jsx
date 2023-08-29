a812c5d1261b062bd603590aec3912518ecba777
adriviris <adrianacarrenog@gmail.com>
2023-08-29 16:40:55 -0400

    parcel

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import { createRoot } from 'react-dom/client';
+
+//IMPORT STATEMENT TO INDICATE THAT YOU NEED TO BUNDLE './INDEX.SCSS'
+import ".index.scss";
+
+//MAIN COMPONENT (WILL EVENTUALLY USE ALL THE OTHERS)
+const MyFlixApplication = () => {
+    return (
+        <div className="my-flix">
+            <div>Good morning</div>
+        </div>
+    );
+};
+
+//FINDS THE ROOT OF YOUR APP
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+//TELLS REACT TO RENDER YOUR APP IN THE ROOT DOM ELEMENT 
+root.render(<MyFlixApplication />)
