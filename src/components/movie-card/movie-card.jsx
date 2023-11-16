b3283652ea5a84bc7a8593e053a40ccb7da1d0d8
adriviris <adrianacarrenog@gmail.com>
2023-11-16 18:41:40 -0500

    updating buttons w bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,21 +1,27 @@
 import PropTypes from "prop-types"; 
+import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-    <div
-    onClick={() => {
-        onMovieClick(movie);
-    }}
-    >
-        {movie.Title}
-        </div>
-    );
-};
+    return ( 
+        <Card>
+            <Card.Img variant="top" src={book.image} />
+            <Card.Body>
+            <Card.Title>{movie.title}</Card.Title>
+            <Button onClick={() => onMovieClick(movie)} variant="link">
+                Open
+            </Button>
+            </Card.Body>
+            </Card>
+            );
+        };
+    
 
 MovieCard.propTypes = {
     movie: PropTypes.shape({
-      Title: PropTypes.string.isRequired,
-      ImagePath: PropTypes.string.isRequired,
+        Title: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string.isRequired,
 
     }).isRequired,
     onMovieClick: PropTypes.func.isRequired
-  };
+};
